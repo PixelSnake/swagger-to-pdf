9886e72c57f5195fa529859cdab04f0e8e06c2c4
Thomas Neumüller <tn@tresmo.de>
2018-11-19 15:08:37 +0100

    feat(pdf): template and stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 const Cli = require('./command-line')
+const http = require('superagent')
+const fs = require('fs')
+const SwaggerTex = require('./swagger-tex')
 
-function run() {
+async function run() {
     Cli.argument.default('swagger.json').short('s').long('source').description('The swagger.json source (either a file path or an URL)')
     Cli.argument.long('user').description('A username for authentication, if necessary')
     Cli.argument.long('pass').description('A password for authentication, if necessary')
+    Cli.flag.bool.default(false).short('v').long('verbose')
 
     const args = Cli.arguments
-    console.log(args);
+
+    const file = await getFile(args.source)
+        .catch(err => console.log(err))
+
+    const doc = JSON.parse(file)
+    SwaggerTex.generate(doc, args)
+}
+
+async function getFile(uri) {
+    if (!uri.startsWith('http')) {
+        return fs.readFileSync(uri).toString()
+    } else {
+        return http.get(uri)
+    }
 }
 
 module.exports = {
---
+++ src/swagger-tex.js
@@ -0,0 +1,120 @@
+const fs = require('fs')
+const path = require('path')
+const _flattenDeep = require('lodash/flattenDeep')
+const mustache = require('mustache')
+const md5 = require('md5')
+const { spawn } = require('child_process')
+
+function generate(json, args) {
+    const template = fs.readFileSync(
+        path.join(__dirname, '..', 'tex', 'doc.tex.mustache')
+    ).toString()
+
+    const title = json.info.title
+    const version = json.info.version
+
+    const tags = json.tags.map(t => {
+        const paths = Object.keys(json.paths)
+            .map(k => {
+                return Object.keys(json.paths[k])
+                    .filter(method => {
+                        return (json.paths[k][method].tags || ['default']).includes(t.name)
+                    })
+                    .map(method => {
+                        const route = json.paths[k][method]
+                        
+                        return {
+                            path: k,
+                            method,
+                            ...route,
+                            label: md5(`${method}:${k}`),
+                            responses: Object.keys(route.responses).map(status => {
+                                const response = route.responses[status]
+                                let schema = '-'
+                                if (response.schema && response.schema.$ref) {
+                                    schema = response.schema.$ref.split('/')[2]
+                                } else if (response.schema) {
+                                    schema = JSON.stringify(response.schema)
+                                }
+                                
+                                
+                                return {
+                                    status,
+                                    ...response,
+                                    schema
+                                }
+                            })
+                        }
+                    })
+            })
+            .filter(p => p.length > 0)
+
+        return {
+            ...t,
+            paths: _flattenDeep(paths)
+        }
+    })
+
+    const definitions = Object.keys(json.definitions)
+        .map(def => {
+            const definition = json.definitions[def]
+            return {
+                name: def,
+                ...definition,
+                properties: definition.properties && Object.keys(definition.properties)
+                    .map(prop => {
+                        const property = definition.properties[prop]
+                        return {
+                            name: prop,
+                            ...property
+                        }
+                    })
+            }
+        })
+    console.log(tags[0]);
+
+    const view = {
+        title,
+        version,
+        tags,
+        definitions
+    }
+    
+    mustache.escape = texEscape
+    const doc = mustache.render(
+        template,
+        view,
+        {},
+        ['!(', ')']
+    )
+
+    fs.writeFileSync(
+        path.join(__dirname, '..', 'tex', 'doc.tex'),
+        doc
+    )
+
+    const texDir = path.join(__dirname, '..', 'tex')
+    process.chdir(texDir)
+
+    const pdflatex = spawn('xelatex', ['doc.tex'])
+    pdflatex.stdout.on('data', data => {
+        if (args.verbose) console.log(data.toString());
+    })
+    pdflatex.stderr.on('data', data => {
+        if (args.verbose) console.log(data.toString());
+    })
+}
+
+module.exports = {
+    generate
+}
+
+function texEscape(str) {
+    if (typeof str !== 'string') return str
+    return str
+        .replace(/{/g, '\\{')
+        .replace(/}/g, '\\}')
+        .replace(/#/g, '\\#')
+        .replace(/\$/g, '\\$')
+        .replace(/"/g, '\'\'')
+}
